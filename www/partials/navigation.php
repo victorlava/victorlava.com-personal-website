656169769456bf2100a3ba39af60179e60d37366
Victor Lava <hello@victorlava.com>
2018-01-24 01:25:16 +0200

    copied footer from old website

---
+++ www/partials/navigation.php
@@ -0,0 +1,19 @@
+<nav class="navigation">
+    <ul>
+        <li>
+            <a href="#">
+                Projektai
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <img src="/img/logo.png" width="109" height="38" alt="UI/UX dizaineris">
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                Kontaktai
+            </a>
+        </li>
+    </ul>
+</nav><!-- .navigation -->
