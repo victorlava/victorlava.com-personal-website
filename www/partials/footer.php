a7d8ba34cc8fddaf297991b314decd983d5a9dc1
Victor Lava <hello@victorlava.com>
2018-01-24 01:28:32 +0200

    moved footer to partials

--- www/partials/footer.php
+++ www/partials/footer.php
@@ -1,3 +1,68 @@
+	<footer class="container">
+		<div class="row">
+			<div class="col-3">
+				<a href="/" title="Web Developer &amp; UI Designer">
+					<img src="/img/logo.png" width="67" height="67" alt="Web Developer &amp; UI Designer">
+				</a>
+				<!-- TODO: change quote -->
+				<blockquote>
+					Dizainas nėra tai, kaip tai atrodo ar ką primena. Dizainas yra tai, kaip tai veikia.
+					<footer>- Steve Jobs</footer>
+				</blockquote >
+				<a href="mailto:hello@victorlava.com">hello@victorlava.com</a>
+			</div>
+			<div class="col-3">
+				<!--  TODO: h6 really? -->
+				<h6 class="title">Navigacija</h6>
+				<ul>
+					<li>
+						<a href="/paslaugos/web-dizainas/" title="Web dizaino paslaugos">
+							Paslaugos
+						</a>
+					</li>
+					<li>
+						<a href="/projektai/" title="Interneto projektai">
+							Projektai
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-3">
+				<h6 class="title hidden-xs hidden-sm">
+					Daugiau projektų
+				</h6>
+				<ul>
+					<li>
+						<a href="/apie-mane/" title="Apie mane">
+							Apie mane
+						</a>
+					</li>
+					<li>
+						<a href="/kontaktai/" title="Susisiekti">
+							Susisiekti
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-3">
+				<h6 class="title">Būk socialus</h6>
+				<ul>
+					<li class="facebook">
+						<a title="Facebook" target="_blank" href="https://www.facebook.com/vvictorlava">
+							Facebook
+						</a>
+					</li>
+					<li class="linkedin">
+						<!--  TODO: add linkedin -->
+						<a title="LinkedIn" target="_blank" href="">
+							LinkedIn
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+	
 	<script type="text/javascript" src="http://localhost:35729/livereload.js"></script>
 </body>
 </html>
