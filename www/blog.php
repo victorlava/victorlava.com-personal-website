42a987d2d3474d8c6ab8d189db50f4815f7f5097
Victor Lava <hello@victorlava.com>
2018-06-14 18:12:52 +0300

    added collective sidebar

--- www/blog.php
+++ www/blog.php
@@ -57,7 +57,34 @@
                     </div>
                 </article>
                 <?php endforeach; ?>
+
             </section>
+
+            <aside class="sticky col-3">
+                <div class="featured">
+                    <h3 class="featured-title">Weekly Web Development Goodies</h3>
+                    <div class="featured-content col-md-12">
+                        <div class="featured-image">
+                            <img class="" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/uploads/2015/05/Collective170.png">
+                        </div>
+
+                        <div class="collective-wrapper">
+                            <ul>
+                                <li>
+                                    <a href="#">The Art Of The SVG Filter</a>
+                                </li>
+                                <li>
+                                    <a href="#">10 year of YouTube</a>
+                                </li>
+                                <li>
+                                    <a href="#">Vibrant.js</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="#" class="btn btn-primary btn-square">See more...</a>
+                </div>
+            </aside>
 
         </div>
 
