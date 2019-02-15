6a4093eb7b905af785d2c8b1368f07bc7082c34c
Victor Lava <hello@victorlava.com>
2019-02-15 05:47:39 +0200

    old nav dropped

--- www/partials/navigation.php
+++ www/partials/navigation.php
@@ -1,38 +1,12 @@
 <nav id="navigation" class="navigation">
-
-
-
     <div class="container-fluid">
         <ul class="menu menu-left">
-            <!-- <li class="logo-mobile slide-down d-sm-block d-md-none">
-                <a href="#" class="logo logo-black logo-xs">
-                    <?php include('components/logo.php'); ?>
-                </a>
-            </li> -->
             <li class="logo-wrapper slide-down d-none d-md-block">
                 <a href="index.php" class="logo logo-white logo-xs">
                     <?php include('components/logo.php'); ?>
                 </a>
             </li>
-            <li>
-
-            </li>
-            <li>
-                <a href="portfolio.php" class="slide-in-parent">
-                    Portfolio
-                </a>
-            </li>
-            <li>
-                <a href="blog.php" class="slide-in-parent">
-                    Blog
-                </a>
-            </li>
         </ul>
-        <div id="navigation-open" class="hamburger hamburger--spring" type="button" onclick="app.openNavigation();">
-            <div class="hamburger-box">
-                <div class="hamburger-inner"></div>
-            </div>
-        </div>
     </div>
 </nav><!-- .navigation -->
 <script type="text/javascript">
